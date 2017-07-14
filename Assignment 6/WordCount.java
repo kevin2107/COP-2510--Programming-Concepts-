9cc279568afa9ccbeb11851c5d32934296956f1d
Kevin Ramnauth <Kevin2107@gmail.com>
2017-07-14 14:49:20 -0400

    Assignment 6

--- Assignment 6/WordCount.java
+++ Assignment 6/WordCount.java
@@ -1,13 +1,37 @@
 //"I pledge my Honor that I have not cheated, and will not cheat, on this assignment" Kevin Ramnauth
+import java.util.Scanner;
+import java.io.*;
+import javax.swing.JOptionPane;
 
 public class WordCount
 {
-   public static void main(String[] args)
+   public static void main(String[] args)throws IOException
    {
-   //Declare Constants
-
    //Declare Inputs
-
+   File inputfile = new File("TheRaven.txt");//Opens Files
+   Scanner scan = new Scanner (inputfile);
    //Declare Variables
+   int count=0;
+
+   JOptionPane.showMessageDialog(null, "This code lets a user enter a word and checks\n 'The Raven'" +
+   " to see how many times it appears.");
+
+   String input = JOptionPane.showInputDialog("Enter a word to looks for: ");
+
+   while (scan.hasNext())
+   {
+      String check = scan.next();
+      if (input.equalsIgnoreCase(check))//Scans each word and compares it to the input word
+      {
+         count++;
+      }      
+   }
+   
+   JOptionPane.showMessageDialog(null, "The word " + input + " appears " + count + " times");
+   
+   scan.close();
+
    }
 }
+
+
