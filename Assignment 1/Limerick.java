c806dc4d7170249d5c68218df4acd7f2ab89560b
Kevin Ramnauth <Kevin2107@gmail.com>
2017-07-13 20:08:40 -0400

    test
    Signed-off-by: Kevin Ramnauth <Kevin2107@gmail.com>

---
+++ Assignment 1/Limerick.java
@@ -0,0 +1,15 @@
+//“I pledge my Honor that I have not cheated, and will not cheat, on this assignment” Kevin Ramnauth
+
+public class Limerick
+{
+   public static void main(String[] args)
+   {
+      System.out.print("Here's a little limerick:\n\n"+
+      "\tThere was an old man with a beard,\n"+
+      "\t\twho said \"It is just as I feared!\n"+
+      "\tTwo owls and a hen,\n+"+
+      "\t\tfour larks and a wren,\n"+
+      "\thave all built their nests in my beard!\".\n\n"+
+      "The end.");              
+   }
+}
